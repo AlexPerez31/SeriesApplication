9ff2f87271727a8f233516bee76055b8ad540312
@alexjordanperez31 <alexjordanperez31@gmail.com>
2026-06-18 13:45:10 -0500

    Fix: Cors error

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,10 +17,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://localhost:3000",
         "https://series-application.vercel.app"
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
