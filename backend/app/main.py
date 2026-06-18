014336eeeab73ee600b17a7a07d251b8a132c9f0
@alexjordanperez31 <alexjordanperez31@gmail.com>
2026-06-18 13:30:53 -0500

    Feat: Deploy frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app.include_router(series.router, prefix="/series", tags=["Series"])
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # luego afinamos
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
